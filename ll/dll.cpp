4b4a7757f23aa5511bcd309ea7cc0dfb14e3d8ad
sohamwagale <89479616+sohamwagale@users.noreply.github.com>
2025-03-24 21:40:36 +0530

    dll learning completed

--- ll/dll.cpp
+++ ll/dll.cpp
@@ -39,9 +39,143 @@ void printLL(Node* head){
     }
 }
 
+Node* insHead(Node* head,int data){
+    return new Node(data,head,nullptr);
+}
+
+Node* delHead(Node* head){
+    if(head == nullptr || head->next == nullptr){
+        return nullptr;
+    }
+    Node* temp = head;
+    head= head->next;
+    head->back = nullptr;
+    temp->next = nullptr;
+    delete temp;
+    return head;
+}
+
+Node* insTail(Node* head,int data){
+    if(head == nullptr){
+        return new Node(data,head,nullptr);
+    }
+    Node* trav = head;
+    while(trav->next!=nullptr){
+        trav = trav->next;
+    }
+    trav->next = new Node(data,nullptr,trav);
+    return head;
+}
+
+Node* delTail(Node* head){
+    if(head == nullptr || head->next == nullptr){
+        return nullptr;
+    }
+    Node* trav = head;
+    while(trav->next!= nullptr){
+        trav = trav->next;
+    }
+    Node* ls = trav->back;
+    trav->back = nullptr;
+    ls->next = nullptr;
+    delete trav;
+    return head;
+}
+
+Node* insB4(Node* head,int data,int pos){
+    if(pos == 1){
+        return insHead(head,data);
+    }
+    int size=0;
+    Node* trav = head;
+    while(trav!=nullptr){
+        size++;
+        if(size == pos) break;
+        trav=trav->next;
+    }
+    Node* prev = trav->back;
+    Node* nn =new Node(data,trav,prev);
+    prev->next = nn;
+    trav->back = nn;
+    return head;
+}
+
+Node* delpos(Node* head, int pos){
+    if (!head) return head;
+    if(pos == 1){
+        return delHead(head);
+    }
+    Node* trav = head;
+    int currPos =1;
+    while(trav!=nullptr){
+        currPos++;
+        trav = trav->next;
+        if(currPos == pos) break;
+    }
+    Node* prev = trav->back;
+    Node* front = trav->next;
+    if(front==nullptr){
+        return delTail(head);
+    }
+    prev->next = front;
+    front->back = prev;
+    trav->next = trav->back = nullptr;
+    delete trav;
+    return head;
+}
+
+Node* delB4(Node* head, int pos){
+    if (!head || pos < 2) return head;
+    if(pos == 2){
+        return delHead(head);
+    }
+    Node* trav = head;
+    int currPos =1;
+    while(trav!=nullptr){
+        currPos++;
+        trav = trav->next;
+        if(currPos == pos-1) break;
+    }
+    Node* prev = trav->back;
+    Node* front = trav->next;
+    prev->next = front;
+    front->back = prev;
+    trav->next = trav->back = nullptr;
+    delete trav;
+    return head;
+}
+
+Node* insNodeB4(Node* head,Node* node,int pos){
+    if(!head) return node;
+    if(pos == 1){
+        node->back = nullptr;
+        node->next =head;
+        head->back = node;
+        return node;
+    }    
+    Node* trav = head;
+    int currPos = 1;
+    while(trav && currPos < pos){
+        currPos++;
+        trav = trav->next;
+    }
+
+    if(!trav) return head;
+
+    Node* prev = trav->back;
+    
+    if(prev) prev->next = node; 
+    trav->back = node;
+
+    node->next = trav;
+    node->back = prev;
+    return head;
+
+}
 
 int main(){
     vector<int> arr = {20,30,50,80,90};
     Node* head = arr2dll(arr);
+    head = insNodeB4(head,new Node(12),3);
     printLL(head);
 }
